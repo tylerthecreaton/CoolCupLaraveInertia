5079d54ac4a886193962cbc401182c6d1be8c17b
Thanakorn <obelo-4129@hotmail.com>
2024-12-27 16:27:55 +0700

    ทำหน้า ใบเสร็จ แต่ยังไม่ได้ใส่โค๊ด เพราะทำลิ้งsidebarยัง ไม่ได้

---
+++ resources/js/Pages/Receipt.jsx
@@ -0,0 +1,12 @@
+import SalesHistory from '../components/SalesHistory';
+
+function SalesHistoryPage() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>หน้าประวัติการขาย</h1>
+      <SalesHistory />
+    </div>
+  );
+}
+
+export default SalesHistoryPage;
